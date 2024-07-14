066d9dee35a7b0f7032eb7763927d8c5e7f14dad
nixtoon <127158636+nixtoon@users.noreply.github.com>
2024-07-14 19:00:35 -0400

    crud marcas y categorias

--- administracion/urls.py
+++ administracion/urls.py
@@ -8,4 +8,12 @@ urlpatterns = [
     path('productos/agregar', views.agregar_producto, name='agregar_producto'),
     path('productos/modificar/<int:id>', views.modificar_producto, name='modificar_producto'),
     path('productos/eliminar/<int:id>', views.eliminar_producto, name='eliminar_productos'),
+    path('marcas', views.listar_marcas, name='marcas'),
+    path('marcas/agregar', views.agregar_marca, name='agregar_marca'),
+    path('marcas/modificar/<int:id>', views.modificar_marca, name='modificar_marca'),
+    path('marcas/eliminar/<int:id>', views.eliminar_marca, name='eliminar_marca'),
+    path('categorias', views.listar_categorias, name='categorias'),
+    path('categorias/agregar', views.agregar_categoria, name='agregar_categoria'),
+    path('categorias/modificar/<int:id>', views.modificar_categoria, name='modificar_categoria'),
+    path('categorias/eliminar/<int:id>', views.eliminar_categoria, name='eliminar_categoria'),
 ]
--- administracion/views.py
+++ administracion/views.py
@@ -25,11 +25,12 @@ def admin_login(request):
     return render(request, 'app/admin_login.html')
 
 
-@user_passes_test(lambda u: u.is_staff)
+@user_passes_test(lambda u: u.is_staff, login_url='admin_login')
 def index(request):
     return render(request, 'app/index.html')
 
 
+@user_passes_test(lambda u: u.is_staff, login_url='admin_login')
 def listar_productos(request):
     headers = {'Authorization': settings.API_TOKEN}
     page = request.GET.get('page', 1)
@@ -54,8 +55,9 @@ def listar_productos(request):
     except:
         messages.error(request, 'Error al obtener productos')
         return redirect('index')
-    
 
+
+@user_passes_test(lambda u: u.is_staff, login_url='admin_login')
 def agregar_producto(request):
     headers = {'Authorization': settings.API_TOKEN,}
     
@@ -104,7 +106,7 @@ def agregar_producto(request):
     return render(request, 'app/crud_productos/agregar.html')
 
 
-
+@user_passes_test(lambda u: u.is_staff, login_url='admin_login')
 def modificar_producto(request, id):
     headers = {'Authorization': settings.API_TOKEN}
     media = settings.MEDIA_URL
@@ -166,7 +168,7 @@ def modificar_producto(request, id):
         return redirect('productos')
 
 
-
+@user_passes_test(lambda u: u.is_staff, login_url='admin_login')
 def eliminar_producto(request, id):
     headers = {'Authorization': settings.API_TOKEN}
     try:
@@ -183,3 +185,245 @@ def eliminar_producto(request, id):
             print('No se pudo obtener detalles del error en formato JSON')
         messages.error(request, 'Hubo un error al eliminar')
         return redirect('productos')
+
+
+@user_passes_test(lambda u: u.is_staff, login_url='admin_login')
+def listar_marcas(request):
+    headers = {'Authorization': settings.API_TOKEN}
+
+    try:
+        response = requests.get(f'http://{settings.API_BASE_URL}/get-marcas/', headers=headers)
+        response.raise_for_status()
+        marcas = response.json()
+        
+        data = {
+            'marcas': marcas,
+        }
+        return render(request, 'app/crud_marcas/listar.html', data)
+    except:
+        messages.error(request, 'Error al obtener marcas')
+        return redirect('index')
+
+
+@user_passes_test(lambda u: u.is_staff, login_url='admin_login')
+def agregar_marca(request):
+    headers = {'Authorization': settings.API_TOKEN,}
+    
+    if request.method == 'POST':
+        nom_marca = request.POST.get('nom_marca')
+
+        payload = {
+            'nom_marca': nom_marca
+        }
+
+        try:
+            response = requests.post(
+                f'http://{settings.API_BASE_URL}/create-marca/',
+                data=payload,
+                headers=headers
+            )
+                
+            response.raise_for_status()
+            print(f'Respuesta de la API: {response.json()}')
+            messages.success(request, 'Marca agregado correctamente')
+            return redirect('marcas')
+        
+        except requests.exceptions.HTTPError as e:
+            print('Error al guardar marca ', e)
+            try:
+                print('Detalles del error: ', e.response.json())  # Obtener más detalles del error
+            except ValueError:
+                print('No se pudo obtener detalles del error en formato JSON')
+            messages.error(request, 'Hubo un error')
+            return redirect('marcas')
+        
+    return render(request, 'app/crud_marcas/agregar.html')  
+
+
+@user_passes_test(lambda u: u.is_staff, login_url='admin_login')
+def modificar_marca(request, id):
+
+    headers = {'Authorization': settings.API_TOKEN}
+
+    try:
+        response = requests.get(f'http://{settings.API_BASE_URL}/get-marca/{id}/', headers=headers)
+        response.raise_for_status()
+        marca = response.json()
+
+        if request.method == 'POST':
+            nom_marca = request.POST.get('nom_marca')
+
+            payload = {
+                'nom_marca': nom_marca,
+
+            }
+
+            try:
+                response = requests.put(
+                    f'http://{settings.API_BASE_URL}/update-marca/{id}/',
+                    data=payload,
+                    headers=headers
+                )
+                    
+                response.raise_for_status()
+                print(f'Respuesta de la API: {response.json()}')
+                messages.success(request, 'Marca modificada correctamente')
+                return redirect('marcas')
+            
+            except requests.exceptions.HTTPError as e:
+                print('Error al modificar marca ', e)
+                try:
+                    print('Detalles del error: ', e.response.json())  # Obtener más detalles del error
+                except ValueError:
+                    print('No se pudo obtener detalles del error en formato JSON')
+                messages.error(request, 'Hubo un error')
+                return redirect('marcas')
+
+        data = {
+            'marca': marca,
+        }
+        return render(request, 'app/crud_marcas/modificar.html', data)
+    
+    except requests.exceptions.HTTPError as e:
+        print('Error al obtener marca: ', e)
+        return redirect('marcas')
+
+
+@user_passes_test(lambda u: u.is_staff, login_url='admin_login')
+def eliminar_marca(request, id):
+    headers = {'Authorization': settings.API_TOKEN}
+    try:
+        response = requests.delete(f'http://{settings.API_BASE_URL}/delete-marca/{id}/', headers=headers)
+        response.raise_for_status()
+        messages.success(request, "Eliminado Correctamente")
+        return redirect('marcas')
+    
+    except requests.exceptions.HTTPError as e:
+        print('Error al eliminar marca ', e)
+        try:
+            print('Detalles del error: ', e.response.json())
+        except ValueError:
+            print('No se pudo obtener detalles del error en formato JSON')
+        messages.error(request, 'Hubo un error al eliminar')
+        return redirect('marcas')
+
+
+@user_passes_test(lambda u: u.is_staff, login_url='admin_login')
+def listar_categorias(request):
+    headers = {'Authorization': settings.API_TOKEN}
+
+    try:
+        response = requests.get(f'http://{settings.API_BASE_URL}/get-categorias/', headers=headers)
+        response.raise_for_status()
+        categorias = response.json()
+        
+        data = {
+            'categorias': categorias,
+        }
+        return render(request, 'app/crud_categorias/listar.html', data)
+    except:
+        messages.error(request, 'Error al obtener categorias')
+        return redirect('index')
+
+
+@user_passes_test(lambda u: u.is_staff, login_url='admin_login')
+def agregar_categoria(request):
+    headers = {'Authorization': settings.API_TOKEN,}
+    
+    if request.method == 'POST':
+        nom_categoria = request.POST.get('nom_categoria')
+
+        payload = {
+            'nom_categoria': nom_categoria
+        }
+
+        try:
+            response = requests.post(
+                f'http://{settings.API_BASE_URL}/create-categoria/',
+                data=payload,
+                headers=headers
+            )
+                
+            response.raise_for_status()
+            print(f'Respuesta de la API: {response.json()}')
+            messages.success(request, 'Categoria agregada correctamente')
+            return redirect('categorias')
+        
+        except requests.exceptions.HTTPError as e:
+            print('Error al guardar categoria', e)
+            try:
+                print('Detalles del error: ', e.response.json())  # Obtener más detalles del error
+            except ValueError:
+                print('No se pudo obtener detalles del error en formato JSON')
+            messages.error(request, 'Hubo un error')
+            return redirect('categorias')
+        
+    return render(request, 'app/crud_categorias/agregar.html')  
+
+
+@user_passes_test(lambda u: u.is_staff, login_url='admin_login')
+def modificar_categoria(request, id):
+
+    headers = {'Authorization': settings.API_TOKEN}
+
+    try:
+        response = requests.get(f'http://{settings.API_BASE_URL}/get-categoria/{id}/', headers=headers)
+        response.raise_for_status()
+        categoria = response.json()
+
+        if request.method == 'POST':
+            nom_categoria = request.POST.get('nom_categoria')
+
+            payload = {
+                'nom_categoria': nom_categoria,
+            }
+
+            try:
+                response = requests.put(
+                    f'http://{settings.API_BASE_URL}/update-categoria/{id}/',
+                    data=payload,
+                    headers=headers
+                )
+                    
+                response.raise_for_status()
+                print(f'Respuesta de la API: {response.json()}')
+                messages.success(request, 'Categoria modificada correctamente')
+                return redirect('categorias')
+            
+            except requests.exceptions.HTTPError as e:
+                print('Error al modificar categoria ', e)
+                try:
+                    print('Detalles del error: ', e.response.json())  # Obtener más detalles del error
+                except ValueError:
+                    print('No se pudo obtener detalles del error en formato JSON')
+                messages.error(request, 'Hubo un error')
+                return redirect('categorias')
+
+        data = {
+            'categoria': categoria,
+        }
+        return render(request, 'app/crud_categorias/modificar.html', data)
+    
+    except requests.exceptions.HTTPError as e:
+        print('Error al obtener categoria: ', e)
+        return redirect('categorias')
+    
+
+@user_passes_test(lambda u: u.is_staff, login_url='admin_login')
+def eliminar_categoria(request, id):
+    headers = {'Authorization': settings.API_TOKEN}
+    try:
+        response = requests.delete(f'http://{settings.API_BASE_URL}/delete-categoria/{id}/', headers=headers)
+        response.raise_for_status()
+        messages.success(request, "Eliminado Correctamente")
+        return redirect('categorias')
+    
+    except requests.exceptions.HTTPError as e:
+        print('Error al eliminar categoria ', e)
+        try:
+            print('Detalles del error: ', e.response.json())
+        except ValueError:
+            print('No se pudo obtener detalles del error en formato JSON')
+        messages.error(request, 'Hubo un error al eliminar')
+        return redirect('categorias')
+    
